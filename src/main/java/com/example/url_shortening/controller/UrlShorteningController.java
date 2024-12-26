9a72c5666d594600a6a6470e557dd94d65bd0493
nandoant <fernandoantonio@protonmail.com>
2024-12-26 10:57:40 -0300

    created: POST method

--- src/main/java/com/example/url_shortening/controller/UrlShorteningController.java
+++ src/main/java/com/example/url_shortening/controller/UrlShorteningController.java
@@ -1,14 +1,38 @@
 package com.example.url_shortening.controller;
 
+import com.example.url_shortening.model.Url;
+import com.example.url_shortening.model.dto.UrlDto;
+import com.example.url_shortening.model.dto.UrlErrorResponseDto;
+import com.example.url_shortening.model.dto.UrlResponseDto;
 import com.example.url_shortening.service.UrlService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class UrlShorteningController {
 
     @Autowired
     private UrlService urlService;
 
+    @PostMapping("/generate")
+    public ResponseEntity<?> generateShortLink(@RequestBody UrlDto urlDto) {
+        Url newShortLink = urlService.generateShortLink(urlDto);
 
+        if (newShortLink != null) {
+            UrlResponseDto urlResonse = new UrlResponseDto();
+            urlResonse.setOriginalUrl(newShortLink.getLongUrl());
+            urlResonse.setShortLink(newShortLink.getShortUrl());
+            urlResonse.setExpirationDate(newShortLink.getExpirationDate());
+            return new ResponseEntity<UrlResponseDto>(urlResonse, HttpStatus.OK);
+        }
+
+        UrlErrorResponseDto urlErrorResponse = new UrlErrorResponseDto();
+        urlErrorResponse.setStatus("404");
+        urlErrorResponse.setError("There was an error generating the link. Please try again.");
+        return new ResponseEntity<UrlErrorResponseDto>(urlErrorResponse, HttpStatus.OK);
+    }
 }
