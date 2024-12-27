0fe38d90b39de196519f743d1adb455b2f3baa1b
nandoant <fernandoantonio@protonmail.com>
2024-12-27 18:55:08 -0300

    refactor: enhance UrlDto with validation annotations and URL validation method

--- src/main/java/com/example/url_shortening/model/dto/UrlDto.java
+++ src/main/java/com/example/url_shortening/model/dto/UrlDto.java
@@ -1,9 +1,22 @@
 package com.example.url_shortening.model.dto;
 
 
+import java.net.URL;
+import com.example.url_shortening.config.UrlConfig;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
 public class UrlDto {
+    @NotBlank(message = "URL cannot be empty or blank")
     private String url;
+
     private String suggestedShortLink;
+
+    @Min(value = 0, message = "Expiration time cannot be negative")
+    @Max(value = UrlConfig.MAX_EXPIRATION_TIME_SECONDS,
+    message = "Expiration time cannot be greater than "+ UrlConfig.MAX_EXPIRATION_TIME_SECONDS +" seconds")
+
     private long expirationTimeInSeconds;
 
     public UrlDto(String url, long expirationTimeInSeconds, String shortLink) {
@@ -30,6 +43,15 @@ public class UrlDto {
 
     public long getExpirationTimeInSeconds() {
         return expirationTimeInSeconds;
+    }
+
+    public boolean isValidUrl() {
+        try {
+            new URL(url).toURI();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     public void setExpirationTimeInSeconds(long expirationTimeInSeconds) {
