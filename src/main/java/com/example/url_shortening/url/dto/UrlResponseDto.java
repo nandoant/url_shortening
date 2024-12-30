de99e264fdaa93570103f31b250229711fd8d091
nandoant <fernandoantonio@protonmail.com>
2024-12-30 17:25:54 -0300

    add: enhance UrlResponseDto with Swagger annotations for improved API documentation

--- src/main/java/com/example/url_shortening/url/dto/UrlResponseDto.java
+++ src/main/java/com/example/url_shortening/url/dto/UrlResponseDto.java
@@ -2,12 +2,22 @@ package com.example.url_shortening.url.dto;
 
 import java.time.LocalDateTime;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
+@Schema(name = "UrlResponseDto", description = "Response DTO for URL response")
 public class UrlResponseDto {
+    @Schema(description = "Original URL",
+            example = "https://www.example.com")
     private String originalUrl;
+
+    @Schema(description = "Shortened URL",
+            example = "abc123")
     private String shortLink;
+
+    @Schema(description = "Expiration date of the shortened URL",
+            example = "2025-12-12T23:59:59")
     private LocalDateTime expirationDate;
 
     public UrlResponseDto(String originalUrl, String shortLink, LocalDateTime expirationDate) {
