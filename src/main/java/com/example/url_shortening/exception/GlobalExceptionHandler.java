006c73932b925f094ceddc851b6482225adfcf5c
nandoant <fernandoantonio@protonmail.com>
2025-01-01 11:15:28 -0300

    feat: add validation error handling in GlobalExceptionHandler

--- src/main/java/com/example/url_shortening/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/url_shortening/exception/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package com.example.url_shortening.exception;
 import com.example.url_shortening.url.exception.UrlErrorCode;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,6 +15,23 @@ import lombok.extern.slf4j.Slf4j;
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<UrlErrorResponseDto> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        String errorMessage = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(FieldError::getDefaultMessage)
+                .findFirst()
+                .orElse("Validation error");
+
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(new UrlErrorResponseDto(
+                HttpStatus.BAD_REQUEST.value(),
+                errorMessage
+            ));
+    }
 
     @ExceptionHandler(BaseException.class)
     public ResponseEntity<UrlErrorResponseDto> handleUrlExceptions(BaseException ex) {
