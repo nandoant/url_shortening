b9265fc033c35ad37dae3d1ea18abe6f9340164a
nandoant <fernandoantonio@protonmail.com>
2025-01-05 00:23:30 -0300

    refactor: simplify Url entity by using Lombok annotations for boilerplate code reduction

--- src/main/java/com/example/url_shortening/url/entity/Url.java
+++ src/main/java/com/example/url_shortening/url/entity/Url.java
@@ -2,18 +2,29 @@ package com.example.url_shortening.url.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Data
+@Builder
+@NoArgsConstructor
 public class Url {
+
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     @Lob
     private String longUrl;
+
     private String shortUrl;
     private LocalDateTime creationDate;
     private LocalDateTime expirationDate;
@@ -24,101 +35,6 @@ public class Url {
         this.shortUrl = shortUrl;
         this.creationDate = creationDate;
         this.expirationDate = expirationDate;
-    }
-
-    public Url() {
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getLongUrl() {
-        return longUrl;
-    }
-
-    public void setLongUrl(String originalUrl) {
-        this.longUrl = originalUrl;
-    }
-
-    public String getShortUrl() {
-        return shortUrl;
-    }
-
-    public void setShortUrl(String shortUrl) {
-        this.shortUrl = shortUrl;
-    }
-
-    public LocalDateTime getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(LocalDateTime creationDate) {
-        this.creationDate = creationDate;
-    }
-
-    public LocalDateTime getExpirationDate() {
-        return expirationDate;
-    }
-
-    public void setExpirationDate(LocalDateTime expirationDate) {
-        this.expirationDate = expirationDate;
-    }
-
-    @Override
-    public String toString() {
-        return "Url{" +
-                "id=" + id +
-                ", originalUrl='" + longUrl + '\'' +
-                ", shortUrl='" + shortUrl + '\'' +
-                ", creationDate=" + creationDate +
-                ", expirationDate=" + expirationDate +
-                '}';
-    }
-
-    public static class UrlBuilder {
-        private long id;
-        private String longUrl;
-        private String shortUrl;
-        private LocalDateTime creationDate;
-        private LocalDateTime expirationDate;
-
-        public UrlBuilder id(long id) {
-            this.id = id;
-            return this;
-        }
-
-        public UrlBuilder longUrl(String longUrl) {
-            this.longUrl = longUrl;
-            return this;
-        }
-
-        public UrlBuilder shortUrl(String shortUrl) {
-            this.shortUrl = shortUrl;
-            return this;
-        }
-
-        public UrlBuilder creationDate(LocalDateTime creationDate) {
-            this.creationDate = creationDate;
-            return this;
-        }
-
-        public UrlBuilder expirationDate(LocalDateTime expirationDate) {
-            this.expirationDate = expirationDate;
-            return this;
-        }
-
-        public Url build() {
-            return new Url(id, longUrl, shortUrl, creationDate, expirationDate);
-        }
-    }
-
-    public static UrlBuilder builder() {
-        return new UrlBuilder();
     }
 
 }
