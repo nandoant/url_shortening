cf3a92aaf186ad7b11d6e76ab839b48ffdbfe109
nandoant <fernandoantonio@protonmail.com>
2025-01-06 10:14:00 -0300

    refactor: enhance ServiceLayerException with equals and hashCode methods

--- src/main/java/com/example/url_shortening/exception/exceptions/ServiceLayerException.java
+++ src/main/java/com/example/url_shortening/exception/exceptions/ServiceLayerException.java
@@ -1,9 +1,11 @@
 package com.example.url_shortening.exception.exceptions;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.springframework.http.HttpStatus;
 
 @Data
+@EqualsAndHashCode(callSuper=false)
 public class ServiceLayerException extends RuntimeException {
     private HttpStatus statusCode;
     private String message;
